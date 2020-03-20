c2b58beb42e34182c1bd3b311461d3a5298859c3
llebronc <l.lebron.c@gmail.com>
2020-03-20 12:40:09 +0000

    t

--- part1.py
+++ part1.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 
 
 def build_model(input_shape=(224,224,3), classes=6):
-    x = keras.layers.Input(input_shape)
-    x = keras.layers.Conv2D(32, (3, 3),activation='relu',padding='same')(x)
+    inputs = keras.layers.Input(input_shape)
+    x = keras.layers.Conv2D(32, (3, 3),activation='relu',padding='same')(inputs)
     x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
     x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), )(x)
 
@@ -19,11 +19,15 @@ def build_model(input_shape=(224,224,3), classes=6):
     x = keras.layers.Flatten()(x)
     x = keras.layers.Dense(512, activation='relu')(x)
     x = keras.layers.Dense(classes, activation='relu')(x)
-    return x
+    return keras.Model(inputs, x)
 
+
+batch_size=64
+lr=1e-6
+decay=0
 
 datagen = keras.preprocessing.image.ImageDataGenerator()
-batch_size=64
+
 # load and iterate training dataset
 train_it = datagen.flow_from_directory('imagenette_6class/train/',target_size=(224,224), class_mode='categorical', batch_size=batch_size)
 # load and iterate validation dataset
@@ -32,6 +36,13 @@ val_it = datagen.flow_from_directory('imagenette_6class/validation/',target_size
 test_it = datagen.flow_from_directory('imagenette_6class/test/',target_size=(224,224), class_mode='categorical', batch_size=batch_size)
 
 model = build_model()
+
+opt = keras.optimizers.Adam(lr=lr,decay=decay)
+
+
+model.compile(optimizer=opt,
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 model.fit_generator(train_it, steps_per_epoch=16, validation_data=val_it, validation_steps=8)
 
