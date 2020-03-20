c06d566d6f96be361328b7d190e13d22c468591c
llebronc <l.lebron.c@gmail.com>
2020-03-20 11:44:17 +0000

    t

---
+++ part1.py
@@ -0,0 +1,39 @@
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def build_model(input_shape=(224,224,3), classes=6):
+    x = keras.layers.Input(input_shape)
+    x = keras.layers.Conv2D(32, (3, 3),activation='relu',padding='same')(x)
+    x = keras.layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2), )(x)
+
+    x = keras.layers.Conv2D(64, (3, 3),activation='relu',padding='same')(x)
+    x = keras.layers.Conv2D(64, (3, 3),activation='relu',padding='same')(x)
+    x = keras.layers.MaxPooling2D((2, 2), strides=(2, 2))(x)
+
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(512, activation='relu')(x)
+    x = keras.layers.Dense(classes, activation='relu')(x)
+    return x
+
+
+datagen = keras.preprocessing.image.ImageDataGenerator()
+batch_size=64
+# load and iterate training dataset
+train_it = datagen.flow_from_directory('imagenette_6class/train/',target_size=(224,224), class_mode='categorical', batch_size=batch_size)
+# load and iterate validation dataset
+val_it = datagen.flow_from_directory('imagenette_6class/validation/',target_size=(224,224), class_mode='categorical', batch_size=batch_size)
+# load and iterate test dataset
+test_it = datagen.flow_from_directory('imagenette_6class/test/',target_size=(224,224), class_mode='categorical', batch_size=batch_size)
+
+model = build_model()
+
+model.fit_generator(train_it, steps_per_epoch=16, validation_data=val_it, validation_steps=8)
+
+model.save('output/part1a.h5')
+# loss = model.evaluate_generator(test_it, steps=24)
